a0a55dc450910c85ccb4cc6fef911d392ee2ee3d
Looper <aayush.bhattacharjee2002@gmail.com>
2023-02-21 19:59:01 +0530

    DSA3

--- DSA3.c
+++ DSA3.c
@@ -5,3 +5,15 @@ int main(int argc, char *argv)
 
     return 0;
 }
+
+// Big O upper bound
+
+// 0 <= f(n) <= c*g(n) for all n >= n0
+
+// Big Omega lower bound
+
+// 0 <= c*g(n) <= f(n) for all n >= n0
+
+// Big Theta upper bound and lower bound
+
+// 0 <= c1*g(n) <= f(n) <= c2*g(n) for all n >= n0 n
