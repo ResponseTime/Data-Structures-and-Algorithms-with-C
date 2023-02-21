2c7a70088bd9d94dc2585f0d621c4934c5ba0e57
Looper <aayush.bhattacharjee2002@gmail.com>
2023-02-21 09:04:33 +0530

    DSA1,2

---
+++ DSA1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int a = 10;
+    int *p = &a;
+    printf("%d", *p);
+    return 0;
+}
+
+// Data structure = way to arrange data in main memory for efficient usage.
+// Algorithm = ser of steps to solve a problem
+// Database =  collection of data for faster retrieval and updation
+// Data Warehouse = collection of data from different sources for analysis
+// Data Mining = process of extracting useful information from large data sets
+
+// |--------------------------------|
+
+//               heap
+
+// |--------------------------------|
+
+//               stack
+
+// |--------------------------------|
+//        static unstatic vars
+// |--------------------------------|
+//           code segment
+// |--------------------------------|
+
+// Requesting memory without freeing leads to memory leaks
---
+++ DSA2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char *argv)
+{
+
+    return 0;
+}
+
+// Big O : order of
+
+// O(n)
+// O(n log n)
+// O(log n)
+// O(n^2)
+// O(n^3)
+// O(n!)
+// O(1)
