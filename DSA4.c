5aa51b278fe8920a6fc0b939a3384e4d39beec7d
Looper <aayush.bhattacharjee2002@gmail.com>
2023-02-22 12:52:41 +0530

    DSA4

---
+++ DSA4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
+
+// arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+// linear search
+// best case = O(1)
+// worst case = O(n)
+// Average case = O(summaition of possible runtimes / number of possible runtimes) O(n)
+
+// binary search
+// best case = O(1)
+// worst case = O(log n)
+// Average case = O(log n)
+// The arr needs to be in sorted order
+
+// log = Number of times a number can be halfed till its exhausted
+// log8 = 3
+// log16 = 4
+
+// space complexity = an array of n elements O(n) space complexity
+// space complexity of a recursive function = O(n)
