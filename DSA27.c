209cc4811b3654d29a5c2002296ce265ccdf74e6
Looper <aayush.bhattacharjee2002@gmail.com>
2023-04-12 16:57:20 +0530

    DSA27

---
+++ DSA27.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
+
+// root - topmost node
+// parent - node which connects to the child node
+// child - node which connects to parent
+// siblings - node belonging to the same parent
+// ancestors - parents of parents
+// descendants - childs of childs
+// leaf - node with no children
+// internal node - node with atleast one child
+// depth - number of edges from the root node
+// height - number of edges from node to the deepest leaf
