86204a77723f4aa4daba0d1e6076b6c36222326f
Looper <aayush.bhattacharjee2002@gmail.com>
2023-04-18 15:42:14 +0530

    DSA32

---
+++ DSA32.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
+
+// traversal in binary tree
+
+// 1) Preorder traversal
+// root -> left subtree -> right subtree
+
+// 2) Postorder traversal
+// left subtree -> right subtree -> root
+
+// 3) Inorder traversal
+// left subtree -> root -> right subtree
