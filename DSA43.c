6d1b0e0e3259332ff8407e63161285f33b00a031
Looper <aayush.bhattacharjee2002@gmail.com>
2023-05-05 15:36:39 +0530

    DSA43

---
+++ DSA43.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
+// deletion of node
+// 1) leaf node
+// search and delete
+// 2) node with children
+// replace node with inorder predecessor or successor
+// 3) root node
+// search node
+// search for inorder predecessor or successor
