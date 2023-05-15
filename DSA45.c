2d1d2ae62df23705bf9a554579194966005f5ee4
Looper <aayush.bhattacharjee2002@gmail.com>
2023-05-15 17:45:09 +0530

    DSA45

---
+++ DSA45.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
+// avl tree is height balanced tree
+// balaned factor = height of right subtree - height of left subtree
+// if all nodes have balanced factor in [1,0,-1] it is said to be an avl tree
+// if we insert an element in an avl tree the balance factor of nodes might get unbalanced to combat that we use rotations'
