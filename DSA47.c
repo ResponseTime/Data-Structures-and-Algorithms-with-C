c88c2b8fad5b8f1050da48ea2980a85ae7ebb3b6
Looper <aayush.bhattacharjee2002@gmail.com>
2023-05-17 16:15:04 +0530

    DSA47

---
+++ DSA47.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
+
+// // if l rotate a tree then r rotate a tree we get the original tree we started with
+// ll rotate r with imbalanced node
+// rr rotate l with imbalanced node
+// lr rotate left child of first imbalanced node and right roate with imbalanced node
+// rl rotate right child of first imbalanced node and left roate with imbalanced node
