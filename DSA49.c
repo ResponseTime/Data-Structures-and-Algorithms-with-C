ee4a7678e7cbbf5ccc62e368d7ea9c91c77258c6
Looper <aayush.bhattacharjee2002@gmail.com>
2023-05-20 15:14:34 +0530

    DSA49

---
+++ DSA49.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
+
+// graph
+// non linear data structure
+// collection of nodes connected by edges
+// G = (V, E)
+// used to model paths, social networks etc...
+// directed edge - one way connection
+// undirected edge - two ways connection
+// indegree - number of edges coming into a node
+// outdegree - number of edges going out of a node
