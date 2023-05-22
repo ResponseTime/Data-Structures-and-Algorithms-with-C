4665d7d21e70ee166a4990feff45208e937e89d5
Looper <aayush.bhattacharjee2002@gmail.com>
2023-05-22 15:24:16 +0530

    DSA50

---
+++ DSA50.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
+
+// representation of graphs
+
+// adjacency list - mark the nodes with the list of its neighbors
+// adjacency matrix - Aij if there is an edge between i and j (1) else 0
+// cost adjacency matrix - Aij if there is an edge between i and j (cost) else 0// if cost is 0 in any case we change all 0s to -1 instead the cost 0
+// edge set - pair of nodes connected by edges
