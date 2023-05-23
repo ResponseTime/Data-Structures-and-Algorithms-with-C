e86a5011b90ef0ad2af0d73c4cc7822cc28ae92b
Looper <aayush.bhattacharjee2002@gmail.com>
2023-05-23 15:28:47 +0530

    DSA51

---
+++ DSA51.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
+
+// graph traversal = visiting all nodes in a graph for updation and deletion
+// DFS - depth first search
+// BFS - breadth first search
+// Queue ds is used in bfs
+// stack ds is used in dfs
