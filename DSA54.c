4ff2ca2c165551623d600d8aa2af80d9fadd0794
Looper <aayush.bhattacharjee2002@gmail.com>
2023-05-26 10:06:32 +0530

    DSA54

---
+++ DSA54.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
+
+// dfs
+// take one node and try to go to the leaf node
+// put any one node on the stack
+// take top item and add it to visited list
+// add all the unvisited adjacent nodes to the stack
+// repeat until stack is empty
