9ab7c9688c98390e9a96a0d09d4110b2403292bf
Looper <aayush.bhattacharjee2002@gmail.com>
2023-05-31 16:11:15 +0530

    DSA56,57

---
+++ DSA56.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
+// spanning trees
+// subgraph
+// a graph whose vertices and edges are subsets of original graph g
+
+// connected graph - a graph that is connected in a sense of a topological space, there is a path from any node to any other node
+// completed graph - an undirected graph in which every pair of vertices is connected by a unique edge
+// spanning tree - should be a subgraph and a connected graph
+// all vertices of G must be present in S
+// No. of edges in S should be V-1
+
+// complete graph has n^n-2 spanning trees (n is the no. of vertices)
---
+++ DSA57.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
+
+// cost spanning tree - sum of all the edges in the spanning tree
+// minimum spanning tree - spanning tree with minimum cost
